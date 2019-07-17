7f95ff531e5e69067517e8e8bafc482f3b74baa9
camalzade_elvin <camalzade_elvin@outlook.com>
2019-07-17 16:50:05 +0500

    okay

--- ChatInConsoleServerside/Program.cs
+++ ChatInConsoleServerside/Program.cs
@@ -10,7 +10,6 @@ namespace ChatInConsoleServerside
 {
     class Program
     {
-        static byte[] buffer = new byte[1024];
         public static int General_Id = 0;
         static void Main(string[] args)
         {
@@ -19,7 +18,7 @@ namespace ChatInConsoleServerside
             //var imagebytes=((byte[])imageconverter.ConvertTo(image, typeof(byte[])));
             Console.WriteLine("======================SERVER====================");
             #region Client Message
-            IPEndPoint endp = new IPEndPoint(IPAddress.Parse("10.1.16.38"), 1031);
+            IPEndPoint endp = new IPEndPoint(IPAddress.Parse("192.168.1.103"), 1031);
             Socket socket;
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(endp);
@@ -40,79 +39,95 @@ namespace ChatInConsoleServerside
                 }
 
             });
-            Task sender = Task.Run(() =>
-            {
-                while (true)
-                {
-                    string message = Console.ReadLine();
-                    if (message == "quit")
-                    {
-                        foreach (var item in clients)
-                        {
+            //Task sender = Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string message = Console.ReadLine();
+            //        if (message == "quit")
+            //        {
+            //            foreach (var item in clients)
+            //            {
 
-                            item.Client.Shutdown(SocketShutdown.Send);
-                        }
-                        break;
-                    }
-                    else
-                    {
-                        //Task ss = Task.Run(() =>
-                        //{
-                        //    foreach (var item in clients)
-                        //    {
-                        //        if (item.Id == General_Id)
-                        //        {
-                        //            item.Client.Send(Encoding.ASCII.GetBytes(message));
-                        //            Console.WriteLine("Server sent " + message);
-                                    
-                        //        }
-                        //        else
-                        //        {
-                        //            item.Client.Send(Encoding.ASCII.GetBytes(message));
-                        //            Console.WriteLine("Server sent " + message);
+            //                item.Client.Shutdown(SocketShutdown.Send);
+            //            }
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Not good zone");
+            //            Task ss = Task.Run(() =>
+            //            {
+            //                foreach (var item in clients)
+            //                {
+            //                    if (item.Id == General_Id)
+            //                    {
+            //                        item.Client.Send(Encoding.ASCII.GetBytes(message));
+            //                        Console.WriteLine("Server sent " + message);
 
-                        //        }
-                        //        //item.Send(imagebytes);
-                        //    }
-                        //});
-                    }
-                }
-            });
+            //                    }
+            //                    else
+            //                    {
+            //                        item.Client.Send(Encoding.ASCII.GetBytes(message));
+            //                        Console.WriteLine("Server sent " + message);
+
+            //                    }
+            //                    //item.Send(imagebytes);
+            //                }
+            //            });
+            //        }
+            //    }
+            //});
+
             Task receiver = Task.Run(() =>
             {
                 while (true)
                 {
-
-                    Task ss1 = Task.Run(() =>
+                    try
                     {
-                        try
-                        {
 
-                            int length = client.Client.Receive(buffer);
-                            var len = Encoding.ASCII.GetString(buffer, 0, length).Length;
-                            var gId = Convert.ToChar(Encoding.ASCII.GetString(buffer, 0, length)[len - 1]).ToString();
-                            General_Id = Convert.ToInt32(gId);
-                            Console.WriteLine("Id -  > "+General_Id.ToString());
-                            var message = Encoding.ASCII.GetString(buffer, 0, length);
-                            if (General_Id == 0)
-                            {
-                                clients[1].Client.Send(Encoding.ASCII.GetBytes(message));
-                                Console.WriteLine("Server sent " + message);
-                            }
-                            else if (General_Id == 1)
-                            {
-                                clients[0].Client.Send(Encoding.ASCII.GetBytes(message));
-                                Console.WriteLine("Server sent " + message);
-                            }
-                            
-                        }
-                        catch (Exception)
+                        foreach (var item in clients)
                         {
+                            if (item.Client.Available != 0)
+                            {
+                                int length = item.Client.Receive(buffer);
+                                if (length != 0)
+                                {
+                                    var len = Encoding.ASCII.GetString(buffer, 0, length).Length;
+                                    var gId = Convert.ToChar(Encoding.ASCII.GetString(buffer, 0, length)[len - 1]).ToString();
+                                    General_Id = Convert.ToInt32(gId);
+                                    Console.WriteLine("Id -  > " + General_Id.ToString());
+                                    var message = Encoding.ASCII.GetString(buffer, 0, length);
+                                    if (General_Id == 0)
+                                    {
+                                        clients[1].Client.Send(Encoding.ASCII.GetBytes(message));
+                                        Console.WriteLine("Server sent " + message);
+                                    }
+                                    else if (General_Id == 1)
+                                    {
+                                        clients[0].Client.Send(Encoding.ASCII.GetBytes(message));
+                                        Console.WriteLine("Server sent " + message);
+                                    }
+                                    //break;
+                                }
+                            }
+
+
+
+
                         }
-                    });
+
+
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
                 }
             });
-            Task.WaitAll(sender, receiver);
+            receiver.Wait();
+            //  Task.WaitAll(sender, receiver);
             #endregion
         }
 
